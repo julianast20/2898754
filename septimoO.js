71ed38ec7fa01bcdc3ba142b7201fcfbb89031cb
julianast20 <160276228+julianast20@users.noreply.github.com>
2024-03-01 16:50:46 -0500

    Delete septimoO.js

--- septimoO.js
+++
@@ -1,9 +0,0 @@
-console.log("7. Realizar un algoritmo que lea el radio de una circunferencia y escriba la longitud de la misma y el área (pi*r) ^2 del círculo inscrito:");
-console.log("Ingrese el valor del radio")
-let radio=15;
-const PI=3.14;
-
-let longitud=(2*PI*radio);
-let area=(PI*Math.pow(radio,2));
- 
-console.log ("El area es:",area," y la longitud es de:",longitud)
