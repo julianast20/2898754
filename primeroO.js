dd0719adb67fdb542a362d76582f032eff242f2e
julianast20 <160276228+julianast20@users.noreply.github.com>
2024-03-01 16:51:25 -0500

    Delete primeroO.js

--- primeroO.js
+++
@@ -1,9 +0,0 @@
-console.log("1. Calcular el area de un triangulo: ");
-console.log("Ingrese la base: ");
-let base= 10;
-console.log("Ingrese la altura: ");
-let altura= 15;
-
-let resultado=(base*altura)/2;
-
-console.log("El area del triangulo: ",resultado);
