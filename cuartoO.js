7e2a40d367b60144c16931a6c5a24bb0094e9f1f
julianast20 <160276228+julianast20@users.noreply.github.com>
2024-03-01 16:39:50 -0500

    test

---
+++ cuartoO.js
@@ -0,0 +1,6 @@
+console.log("4. Escribir un algoritmo que convierta de euros a dolares: ");
+console.log("Ingrese la cantidad de euros: ");
+let euros= 56;
+
+let resultado=(euros*1.83);
+console.log("El total de los euros en dolares es:", resultado);
