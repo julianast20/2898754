3cf94929265dd9e4ce7018cadbd00f3dc32fd618
julianast20 <160276228+julianast20@users.noreply.github.com>
2024-03-01 16:49:27 -0500

    test

---
+++ ejercicios/quintoO.js
@@ -0,0 +1,8 @@
+console.log("5. Escribir un algoritmo que pida el lado de un cuadrado y muestre el valor de la area y del perimetro: ");
+console.log("Ingrese el lado del cuadrado: ");
+let lado= 10;
+
+let area=(lado*lado);
+let perimetro=(4*lado);
+
+console.log("El perimetro es de:",perimetro, "y el area es:",area);
---
+++ ejercicios/terceroO.js
@@ -0,0 +1,7 @@
+console.log("3. Ingresar un numero y visualizar el numero elevado al cuadrado: ");
+console.log("Ingrese un numero: ");
+let num= 8;
+
+let resultado=Math.pow(num,2);
+
+console.log("El numero elevado al cuadrado es:" ,resultado);
