5a455ffa7df429f71777da5a6af547dba89be684
julianast20 <160276228+julianast20@users.noreply.github.com>
2024-03-01 16:50:53 -0500

    Delete segundoO.js

--- segundoO.js
+++
@@ -1,9 +0,0 @@
-console.log("2. Ingresar dos numeros por teclado y sumarlos: ");
-console.log("Ingrese un numero: ");
-let numero1= 20;
-console.log("Ingrese otro numero: ");
-let numero2= 15;
-
-let resultado=(numero1+numero2);
-
-console.log("La suma de los numeros es: ",resultado);
