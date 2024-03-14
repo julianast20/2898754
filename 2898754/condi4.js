13046e4e29378f2400c46a456185c46b12e9484e
julianast20 <160276228+julianast20@users.noreply.github.com>
2024-03-13 22:49:31 -0500

    test

---
+++ 2898754/condi4.js
@@ -0,0 +1,11 @@
+console.log("4.Dados dos numeros A y B, sumarlos si A es menor que B o si no restarlos:")
+console.log("Ingrese el primer numero: ")
+let numA=8
+console.log("Ingrese el segundo numero: ")
+let numB=10
+
+if (numA<numB){
+    console.log("La suma de los numeros es: ",numA+numB);
+}else{
+    console.log("La resta de los numeros es: ",numA-numB);
+}
